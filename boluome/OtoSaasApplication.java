c2d0f31ad9f878a81ee6e0a80e5ca3bf48a2bbda
liuxin <liuxin@boluome.com>
2017-07-24 17:55:29 +0800

    模板提交4

--- boluome/OtoSaasApplication.java
+++ boluome/OtoSaasApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OtoSaasPayApplication {
+public class OtoSaasApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OtoSaasPayApplication.class, args);
+		SpringApplication.run(OtoSaasApplication.class, args);
 	}
 }
